232b49b29a3160657d2fa505545ee35f0a603476
blumgardt <471752@niuitmo.ru>
2026-02-15 14:45:02 +0300

    feat: detect common log calls

--- analyzer/analyzer.go
+++ analyzer/analyzer.go
@@ -8,6 +8,12 @@ import (
 	"golang.org/x/tools/go/ast/inspector"
 )
 
+var logMethodNames = map[string]struct{}{
+	"Debug": {}, "Info": {}, "Warn": {}, "Error": {}, "Fatal": {}, "Panic": {},
+	"Debugw": {}, "Infow": {}, "Warnw": {}, "Errorw": {}, "Fatalw": {}, "Panicw": {},
+	"Debugf": {}, "Infof": {}, "Warnf": {}, "Errorf": {}, "Fatalf": {}, "Panicf": {},
+}
+
 var Analyzer = &analysis.Analyzer{
 	Name: "loglint",
 	Doc:  "analyzer",
@@ -18,19 +24,29 @@ var Analyzer = &analysis.Analyzer{
 }
 
 func run(pass *analysis.Pass) (interface{}, error) {
-	inspector2 := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+	ins := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
 
 	nodeFilter := []ast.Node{
 		(*ast.CallExpr)(nil),
 	}
 
-	inspector2.Preorder(nodeFilter, func(node ast.Node) {
+	ins.Preorder(nodeFilter, func(node ast.Node) {
 		call := node.(*ast.CallExpr)
 
 		if sel, ok := call.Fun.(*ast.SelectorExpr); ok {
-			if sel.Sel.Name == "Println" {
-				pass.Reportf(sel.Pos(), "found Println call")
+			name := sel.Sel.Name
+			if _, ok := logMethodNames[name]; ok {
+				pass.Reportf(sel.Sel.Pos(), "found candidate log call: %s", name)
 			}
+			return
+		}
+
+		if ident, ok := call.Fun.(*ast.Ident); ok {
+			name := ident.Name
+			if _, ok := logMethodNames[name]; ok {
+				pass.Reportf(ident.Pos(), " found candidate log call: %s", name)
+			}
+			return
 		}
 	})
 
--- example/main.go
+++ example/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"log/slog"
+	"os"
+
+	"go.uber.org/zap"
+)
 
 func main() {
-	fmt.Println("hello")
+	slog.Info("server started", "port", 8080)
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	logger.Error("db failed", "code", 500)
+
+	zl, _ := zap.NewProduction()
+	zl.Info("zap started", zap.Int("port", 8080))
+
+	sugar := zl.Sugar()
+	sugar.Infow("sugar started", "env", "prod")
+	sugar.Infof("user %s logged in", "blumgardt")
 }
